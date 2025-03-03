c2049b3360976d74bab8a8eace16dfcc1752d5a2
nscraft <nscraft@gmail.com>
2025-03-03 13:50:47 -0700

    initial commit

--- orchestration/orchestrate.py
+++ orchestration/orchestrate.py
@@ -1,10 +1,6 @@
 class Orchestrate:
-    def __init__(self, config):
-        self.config = config
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.INFO)
-        self.logger.addHandler(logging.StreamHandler())
+    def __init__(self, **kwargs):
+        pass
 
     def run(self):
-        self.logger.info("Orchestration started")
-        self.logger.info("Orchestration completed")
+        pass
