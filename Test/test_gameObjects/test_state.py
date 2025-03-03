24130d073a350b339cc865fe93d6ed3bb55ae323
nscraft <nscraft@gmail.com>
2025-03-03 13:06:20 -0700

    initial commit

--- Test/test_gameObjects/test_state.py
+++ Test/test_gameObjects/test_state.py
@@ -10,9 +10,16 @@ class TestGameState(unittest.TestCase):
     def test_players_playing(self):
         self.assertTrue(self.game_state.players_playing())
 
+        self.assertFalse(GameState('standard', []).players_playing())
+
     def test_remove_player(self):
         self.game_state.remove_player('player1')
         self.assertEqual(self.game_state.num_players, 1)
+        self.assertEqual(self.game_state.players, ['player2'])
+
+        self.game_state.remove_player('player2')
+        self.assertEqual(self.game_state.num_players, 0)
+        self.assertEqual(self.game_state.players, [])
 
     def test_advance_turn(self):
         self.game_state.phase = 'Beginning Phase'
@@ -20,3 +27,17 @@ class TestGameState(unittest.TestCase):
         self.game_state.advance_turn()
         self.assertEqual(self.game_state.step, 'Upkeep Step')
         self.assertEqual(self.game_state.phase, 'Beginning Phase')
+
+        self.game_state.step = 'Draw Step'
+        self.game_state.advance_turn()
+        self.assertEqual(self.game_state.step, 'Main Phase')
+        self.assertEqual(self.game_state.phase, 'Pre-combat Main Phase')
+        self.game_state.advance_turn()
+        self.assertEqual(self.game_state.step, 'Beginning of Combat Step')
+        self.assertEqual(self.game_state.phase, 'Combat Phase')
+
+        self.game_state.phase = 'Combat Phase'
+        self.game_state.step = 'End of Combat Step'
+        self.game_state.advance_turn()
+        self.assertEqual(self.game_state.step, 'Main Phase')
+        self.assertEqual(self.game_state.phase, 'Post-combat Main Phase')
