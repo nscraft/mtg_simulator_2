9f019c6c9c8a05446172b38a66c02eb8ce9870cf
nscraft <nscraft@gmail.com>
2025-03-07 08:40:55 -0700

    initial commit

--- gameObjects/deck.py
+++ gameObjects/deck.py
@@ -1,8 +1,9 @@
 class Deck:
-    def __init__(self, kind: str, cards: list[str], commander=None):
+    def __init__(self, kind: str, cards: list[str], commander: str | None = None):
         self.kind = kind  # 'commander' or 'standard'
         self.cards = cards  # list of card names
         self.num_cards = len(self.cards)
         assert self.num_cards > -1, 'Deck is empty'
         if kind == 'commander':
+            assert commander is not None, 'Commander deck must have a commander'
             self.commander = commander
